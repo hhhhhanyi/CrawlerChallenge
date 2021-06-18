3fa317133be12df3db527afd8d635d9ef6434e3f
hhhhhanyi <hhhhhanyi@gmail.com>
2021-06-18 09:31:28 +0800

    add deploy flow for report service

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -2,3 +2,5 @@
 
 kubectl create secret generic crawler-app-credential --from-file=crawler-app-credential.json
 kubectl apply -f BlockChainCrawler.cronjob.yml
+kubectl apply -f BlockChainReport.deploy.yml
+kubectl apply -f BlockChainReport.service.yml
